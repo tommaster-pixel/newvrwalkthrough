928c9b8bcc4b87232f333f258076b44cbc3c6c4c
tommaster-pixel <75161674+tommaster-pixel@users.noreply.github.com>
2024-01-03 11:44:40 +0530

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// script.js
+
+// Disable right-click context menu
+document.addEventListener('contextmenu', function (e) {
+    e.preventDefault();
+});
+
+// Other JavaScript code for your application goes here
+
 (function(){
     var script = {
  "borderRadius": 0,
